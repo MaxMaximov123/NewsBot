fcc009b260c2e09a68a879be3a89a5469f3c4fe1
Max Maximov <maxss.k2n@yandex.ru>
2022-02-23 22:49:55 +0300

    update

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 token = "5256387436:AAHU4l8HhLRHBBV-1YZxYwcn1BLSstpvVcs"
 
-links = {"Овен": "https://horo.mail.ru/prediction/aries/today/",
-         "Телец": "https://horo.mail.ru/prediction/taurus/today/",
-         "Близнецы": "https://horo.mail.ru/prediction/gemini/today/",
-         "Рак": "https://horo.mail.ru/prediction/cancer/today/",
-         "Лев": "https://horo.mail.ru/prediction/leo/today/",
-         "Дева": "https://horo.mail.ru/prediction/virgo/today/",
-         "Весы": "https://horo.mail.ru/prediction/libra/today/",
-         "Скорпион": "https://horo.mail.ru/prediction/scorpio/today/",
-         "Стрелец": "https://horo.mail.ru/prediction/sagittarius/today/",
-         "Козерог": "https://horo.mail.ru/prediction/capricorn/today/",
-         "Водолей": "https://horo.mail.ru/prediction/aquarius/today/",
-         "Рыбы": "https://horo.mail.ru/prediction/pisces/today/"}
+links = {"Овен♈": "https://horo.mail.ru/prediction/aries/today/",
+         "Телец♉️": "https://horo.mail.ru/prediction/taurus/today/",
+         "Близнецы♊️": "https://horo.mail.ru/prediction/gemini/today/",
+         "Рак♋️": "https://horo.mail.ru/prediction/cancer/today/",
+         "Лев♌️": "https://horo.mail.ru/prediction/leo/today/",
+         "Дева♍️": "https://horo.mail.ru/prediction/virgo/today/",
+         "Весы♎️": "https://horo.mail.ru/prediction/libra/today/",
+         "Скорпион♏️": "https://horo.mail.ru/prediction/scorpio/today/",
+         "Стрелец♐️": "https://horo.mail.ru/prediction/sagittarius/today/",
+         "Козерог♑️": "https://horo.mail.ru/prediction/capricorn/today/",
+         "Водолей♒️": "https://horo.mail.ru/prediction/aquarius/today/",
+         "Рыбы♓️": "https://horo.mail.ru/prediction/pisces/today/"}
